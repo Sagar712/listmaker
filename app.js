39f3a2106dae396d44e8cef706e1739cb065d44c
Sagar712 <74832706+Sagar712@users.noreply.github.com>
2021-02-14 17:54:08 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -24,17 +24,56 @@ function addval(){
 
 	let lop=0;
 	for(let j=0; j<names.length; j++){
-		str += `<tr><td>${j+1}</td><td>${names[j]}</td><td>${quants[j]}</td><td>${prices[j]}</td><td><button class="clos">X</button></td></tr>`;
+		str += `<tr><td>${j+1}</td><td>${names[j]}</td><td>${quants[j]}</td><td>${prices[j]}</td><td><button onclick = "handler(this.id)"class="clos" id="${j+1}">X</button></td></tr>`;
 	}
 	celldata.innerHTML = str;
 
 }
 
-/*
 
-const closebtn = document.querySelector(".clos");
-closebtn.addEventListener('click', function handler(){
-	console.log("X clicked");
-});
 
-*/
+function handler(id){
+	console.log("X clicked:"+id);
+	let str=`<tr>
+	<th>Sr.</th>
+	<th>Item Name</th>
+	<th>Quatity</th>
+	<th>Price</th>
+	<th>Remove</th>
+</tr>`;
+	let tempname =[];
+	let tempquant =[];
+	let tempprice =[];
+	let x=0;
+	for(let j=0; j<names.length; j++){
+		if(j==(id-1)){
+			i--;
+		}
+		else{
+			tempname[x] = names[j];
+			tempquant[x] = quants[j];
+			tempprice[x++] =prices[j];
+			if(x>=names.length)
+				break;
+		}	
+		
+	}
+	names=tempname;
+	quants=tempquant;
+	prices=tempprice;
+	console.log(names)
+	for(let j=0; j<names.length; j++){
+		str += `<tr><td>${j+1}</td><td>${names[j]}</td><td>${quants[j]}</td><td>${prices[j]}</td><td><button onclick = "handler(this.id)"class="clos" id="${j+1}">X</button></td></tr>`;
+	}
+	celldata.innerHTML = str;
+}
+
+function resetval(){
+
+	document.getElementById("name").value="";
+	document.getElementById("quant").value="";
+	document.getElementById("price").value="";
+
+	
+}
+
